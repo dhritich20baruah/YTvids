e8a1cd41ae90d6cdcf08f4a8d7aceb73c938a540
Delta <dhriti.baruah20@gmail.com>
2023-01-07 19:08:29 +0530

    Intial Commit Packages Installed

---
+++ Dashboard/backend/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express(); //make it executable
+const PORT = 5000;
+
+app.get("/", (req, res)=>{
+    res.send("App is working")
+})
+
+app.listen(PORT, ()=>{console.log(`Server running at port ${PORT}`)})
