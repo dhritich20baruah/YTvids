c6889b9062467f48ff3b386b0f1f197a3dc83446
Delta <dhriti.baruah20@gmail.com>
2023-04-17 21:03:45 +0530

    new Note api created

--- nextjs-crud/src/pages/api/newNote.js
+++ nextjs-crud/src/pages/api/newNote.js
@@ -1,19 +1,38 @@
-import {MongoClient} from "mongodb"
-async function handler(req, res){
-    if(req.method !== 'POST') return
-    const {title, note} = req.body
-    const done = "false"
-    if(!title || !note) return
+// import {MongoClient} from "mongodb"
+const mongoose = require('mongoose')
+import Notes from "../../../model/Notes"
 
-    const client = await MongoClient.connect('mongodb://localhost/nextJSCRUD')
-    const db = client.db()
-    const collection = db.collection('todo')
-    const result = await collection.insertOne({title, note, done})
-    client.close()
-    res.status(201).json({
-        todo: result,
-        message: "To do created"
-    })
+async function handler(req, res) {
+    if (req.method !== 'POST') {
+        return res.status(405).end()
+    }
+
+    try {
+        const { title, note } = req.body
+
+        if (!title || !note) {
+            return res.status(400).json({ error: 'Note is required' })
+        }
+        //Connect to mongodb
+        await mongoose.connect('mongodb://localhost/nextCRUD', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }).then(() => console.log('DB connected')).catch(err => console.log(err))
+
+        const newNote = new Notes({
+            title, note
+        })
+        await newNote.save()
+        console.log(newNote)
+
+        res.status(201).json({ message: 'Note created successfully', note: newNote });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Unable to create note' });
+    } finally {
+        mongoose.connection.close();
+    }
 }
+
 
 export default handler
