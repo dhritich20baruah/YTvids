c83f582b0ab38d3f05f3454de4a12c0a870eb231
Delta <dhriti.baruah20@gmail.com>
2023-04-26 11:47:56 +0530

    notes added

--- nextjs-crud/notes/api/addNote.js
+++ nextjs-crud/notes/api/addNote.js
@@ -8,17 +8,18 @@ async function handler(req, res) {
 
     try {
         const { title, note } = req.body
-
+        //Destructuring the req.body object sent from the front end and extracting the values of title and note and also creating two new variables of the same name.
         //Connect to mongodb
-        await mongoose.connect('mongodb://localhost/nextJSCRUD', {
+        await mongoose.connect('mongodb://localhost/nextCRUD', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         }).then(() => console.log('Database connected')).catch(err => console.log(err))
-
+        //usenewurlparser and useunifiedtopology are MongoDB driver's new connection management engine. and its new urlparser. We should set them both to true . 
         var newNote = new Notes({
             title, note
         })
-        await newNote.save()
+        //newNote is the new instance of the note model created using mongoose with values of title and note.
+        await newNote.save() //.save() is a mongoose command that creates a new record in the collection.
         console.log(newNote)
     } catch (err) {
         console.log(err)
--- nextjs-crud/notes/index.js
+++ nextjs-crud/notes/index.js
@@ -1,5 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import React, { useState } from 'react'
 import { Inter } from 'next/font/google'
 import Axios from 'axios'
