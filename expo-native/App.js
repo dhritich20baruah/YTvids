6c7395b1fb9f8cffaef2a78ebe2cdcf88a12f567
Dhriti <dhriti.baruah20@gmail.com>
2024-03-27 09:22:08 +0530

    native wind added

--- expo-native/App.js
+++ expo-native/App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>App started</Text>
+      <Text className="text-red-600">App started</Text>
       <StatusBar style="auto" />
     </View>
   );
