ccd8bfe84ff937b6176a98aa4ec8c5affd4ab054
Dhriti <dhriti.baruah20@gmail.com>
2025-10-26 20:58:39 +0530

    syncing

--- nextjs-clerk/app/Contact/page.tsx
+++ nextjs-clerk/app/Contact/page.tsx
@@ -1,9 +1,22 @@
+"use client"
 import React from 'react'
+import { useUser } from '@clerk/nextjs';
 
 const Contact = () => {
+  const { isSignedIn, user, isLoaded } = useUser();
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isSignedIn) {
+    return <div>Sign in to view this page</div>;
+  }
+
   return (
      <div className='m-10'>
       <h1 className='text-5xl font-bold text-center'>Contact</h1>
+      <p className="text-center text-xl font-semibold">Hello {user.firstName}!</p>
     </div>
   )
 }
