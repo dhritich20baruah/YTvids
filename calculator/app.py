a8f669638d93783b315473e7f5634dd9bf16d4b2
Dhriti <dhriti.baruah20@gmail.com>
2026-01-11 17:45:40 +0530

    Flask Chapter ended

--- calculator/app.py
+++ calculator/app.py
@@ -1,14 +1,37 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, flash
 from flask_cors import CORS
 import math
 
 app = Flask(__name__)
 
+app.secret_key = "secret_key"
+
 CORS(app)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    name = request.args.get("name", default="NA")
+    lang = request.args.get("lang")
+    flash("Please enter values to calculate", "Success")
+    result = "Prediction: 42"
+    if request.method == 'POST':
+        print(request.form)
+    return render_template("index.html", Prediction=result, items=['Apple', 'Banana', 'Cherry'], name=name, lang=lang)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    feature = request.form.get('feature1')
+    if not feature:
+        flash('Please enter a value')
+        return redirect('/')
+
+    flash('Prediction complete')
+    return redirect('/')
+
+
+@app.route('/index2')
+def index2():
+    return render_template('index2.html')
 
 @app.route("/calculator", methods=["POST"])
 def calculate():
