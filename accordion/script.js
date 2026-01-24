515c37699d772f6cec87bef3cbd719175f2c0ab1
Dhriti <dhriti.baruah20@gmail.com>
2026-01-24 07:46:27 +0530

    new html

---
+++ accordion/script.js
@@ -0,0 +1,37 @@
+const passengers = [
+  {
+    id: 1,
+    passengerName: "Freddie Mercury",
+    isVegetarianOrVegan: false,
+    connectedFlights: 2,
+  },
+  {
+    id: 2,
+    passengerName: "Amy Winehouse",
+    isVegetarianOrVegan: true,
+    connectedFlights: 4,
+  },
+    {
+    id: 3,
+    passengerName: "Kurt Cobain",
+    isVegetarianOrVegan: true,
+    connectedFlights: 3,
+  },
+     {
+    id: 3,
+    passengerName: "Michael Jackson",
+    isVegetarianOrVegan: true,
+    connectedFlights: 1,
+  },
+];
+
+
+let result
+let x = 10
+if (x>5){
+    result = "greater"
+}
+else{
+    result = "no"
+}
+console.log(result)
